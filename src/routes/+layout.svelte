d381e9245753c0f7e5a8a3c82db7467224628b58
evariste1963 <evariste@hotmail.co.uk>
2023-08-06 21:02:10 +0100

    project set-up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<slot />
+
+<style>
+	/*import picocss in layout*/
+	@import 'https://unpkg.com/@picocss/pico@latest/css/pico.min.css';
+
+	:global(body) {
+		padding: 2rem;
+	}
+
+	:global(input, button) {
+		border-radius: 1rem;
+	}
+</style>
