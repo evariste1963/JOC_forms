eb5181ddf0fba032a044d204fb0b4d8f763592a2
evariste1963 <evariste@hotmail.co.uk>
2023-08-08 08:13:24 +0100

    todos route

---
+++ src/routes/todos/+page.server.js
@@ -0,0 +1,7 @@
+import { getTodos } from '../../lib/server/database';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+	const todos = getTodos;
+	return { todos };
+}
---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<pre>
+    {JSON.stringify(data, null, 2)}
+</pre>
