9aead5b48afa034795cdfbb4d74f7d3b15c63cfc
evariste1963 <evariste@hotmail.co.uk>
2023-08-08 07:06:45 +0100

    logini

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 <form method="GET" action="/login">
 	<input type="text" name="user" />
 	<input type="password" name="password" />
-	<button type="submit">Logini</button>
+	<button type="submit">Login</button>
 </form>
