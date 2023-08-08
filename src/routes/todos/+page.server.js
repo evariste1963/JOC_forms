ec21e95ab8514ae6f40601395ab82a96b74464c7
evariste1963 <evariste@hotmail.co.uk>
2023-08-08 08:19:37 +0100

    todos route setup

--- src/routes/todos/+page.server.js
+++ src/routes/todos/+page.server.js
@@ -2,6 +2,6 @@ import { getTodos } from '../../lib/server/database';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const todos = getTodos;
+	const todos = getTodos();
 	return { todos };
 }
