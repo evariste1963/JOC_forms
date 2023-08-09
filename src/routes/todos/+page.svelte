aa8a2ecc8be3d1aacbc1c496cdaa4e73a20d8d21
evariste1963 <evariste@hotmail.co.uk>
2023-08-09 07:04:22 +0100

    3:04:20 adding and removing todos +page sketvhout

--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -1,8 +1,33 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+
+	async function addTodo(event) {
+		console.log(event);
+	}
+
+	async function removeTodo(event) {}
 </script>
 
 <pre>
     {JSON.stringify(data, null, 2)}
 </pre>
+
+<ul>
+	{#each data.todos as todo}
+		<li>
+			<span>{todo.text}</span>
+			<form on:submit|preventDefault={removeTodo} method="POST">
+				<!-- USE FORMS INSTEAD OF BUTTONS!!-- HOWEVER SMALL THE FORM MAYBE!!-->
+				<input type="hidden" name="id" value={todo.id} />
+				<!--HIDDEN INPUT TO SEND TO THE SERVER -->
+				<button class="delete" type="submit">❌</button>
+			</form>
+		</li>
+	{/each}
+</ul>
+
+<form on:submit|preventDefault={addTodo} method="POST">
+	<input type="text" name="todo" />
+	<button type="submit">+ Add Todo</button>
+</form>
